28be8013c76d00185ca85cda0044fe2de02bdf09
Eric Randall <eric.randalls@gmail.com>
2015-04-24 14:29:07 -0400

    Add toolkit
    Started toolkit file with all my favourite analysis measures

---
+++ predict-401/toolkit.r
@@ -0,0 +1,10 @@
+salaries <- read.csv(file.path("C:/Users/00811289/Desktop","salaries.csv"), sep=",")
+
+summary(salaries)
+str(salaries)
+sd(salaries$SAL)
+
+hist(salaries$SAL)
+skewness(salaries$SAL)
+kurtosis(salaries$SAL)
+plot(salaries$AGE, salaries$SAL)
